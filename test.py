a15dc6d9f5e131bb5e558abd4d2b5a1ff5801e09
polykelvin <kelvimchop@gmail.com>
2022-06-14 20:51:46 +0800

    `

--- test.py
+++ test.py
@@ -1,9 +1,4 @@
-print("Hello this is Github")
-print("Hello this is VSCode ")
-print("Nice to meet you VS")
-print("Nice to meet you too Github")
-
-print("Ha how about we talk in other branch?")
-print("wait do you know how to do it???")
-print("I mean both on VS and Github?")
-
+'''
+I dont know (It whisper)
+But I make a new comment
+'''
