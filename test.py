3ec1ad4bc9d6946fcf04aecaa541cb5d201126d7
polykelvin <kelvimchop@gmail.com>
2022-06-14 20:50:05 +0800

    1

--- test.py
+++ test.py
@@ -4,4 +4,6 @@ print("Nice to meet you VS")
 print("Nice to meet you too Github")
 
 print("Ha how about we talk in other branch?")
-print("wait do you know how to do it?")
+print("wait do you know how to do it???")
+print("I mean both on VS and Github?")
+
